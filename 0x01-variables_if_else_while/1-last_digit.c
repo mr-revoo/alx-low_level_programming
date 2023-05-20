99374b8e361bea7f9dfc6d297d66bb64746b8ae9
root <root@DESKTOP-ABLNI7D.localdomain>
2023-05-20 04:58:40 +0300

    please

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
-	  printf("is %d and is greater than 5\n", n % 10);
+		printf("is %d and is greater than 5\n", n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
