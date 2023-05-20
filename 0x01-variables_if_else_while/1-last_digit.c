0ca514e3f02381b592324097c9b1e6aafa49a47f
root <root@DESKTOP-ABLNI7D.localdomain>
2023-05-20 04:52:52 +0300

    trial

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ((n % 10) > 5)
+	{
+	  printf("is %d and is greater than 5\n", n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("is %d and is less than 6 and not 0\n", n % 10);
+	}
+	else if ((n % 10) == 0)
+	{
+		printf("is %d and is 0\n", n % 10);
+	}
+}
