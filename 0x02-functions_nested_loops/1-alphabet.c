edbe31556b089979263c24c64a6e5c2af89309ee
refaat <refaatmohamed61@gmail.com>
2023-07-21 03:39:00 +0300

    hello there

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,12 @@
 
 void print_alphabet(void)
 {
-	for (int i = 'a'; i <= 'z' ; i++)
-		_putchar(i);
+	char let;
 
-		_putchar("\n");
+	for (let = 'a'; let <= 'z' ; let++)
+	{
+		_putchar(let);
+	}
+
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include<stdio.h>
 
 int _puchar(char c);
 void print_alphabet(void);
+
+
+#endif
