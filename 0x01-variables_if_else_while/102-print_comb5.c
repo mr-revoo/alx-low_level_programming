464abcad428e5ca274959f193a7867b105ecdb4f
refaat <refaatmohamed61@gmail.com>
2024-04-05 23:15:10 +0200

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+
 /**
- * main - Entry point
+ *main - Prints all possible combinations of two two-digit numbers,
+ *        ranging from 0-99, separated by a comma followed by a space.
  *
- * Description: Printing all possible different combinations of three digits
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, j, k, u;
+int i, j;
 
-	for (i = '0'; i <= '9'; i++)
-		for (j = '0'; j <= '9'; j++)
-			for (k = '0'; k <= '9'; k++)
-				for (u = '0'; u <= '9'; u++)
-					if (((i <= k) && (j < u)) || ((i < k) && (j <= u)))
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(k);
-						putchar(u);
-						if (i != 57 || j != 56)
-						{
-						putchar(',');
-						putchar(' ');
-						}
-					}
-
-
-	
-	putchar('\n');
-	return (0);
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
