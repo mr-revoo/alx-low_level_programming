dab22e3104a7263ab822172e0fc1dd1f9ad65233
refaat <refaatmohamed61@gmail.com>
2024-04-05 21:45:16 +0200

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - MainFunction
+* Return: 0
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i++);
+	}
+	printf("\n");
+	return (0);
+}
